56f6abe7e4d835100a4c559bab8d0cc2e1ea4542
eM-eSeM <79427809+eM-eSeM@users.noreply.github.com>
2023-04-28 19:11:02 -0400

    Updated startingData.sql
    Updated Inventory in startingData.sql, added images for lab icon folders

--- database/schema.sql
+++ database/schema.sql
@@ -2,8 +2,6 @@ CREATE TABLE inventory (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     item_name VARCHAR(255) NOT NULL,
     info VARCHAR(255) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    stock INTEGER NOT NULL,
     image_url VARCHAR(255) NOT NULL,
     category VARCHAR(255) NOT NULL
 );
@@ -23,7 +21,6 @@ CREATE TABLE sales (
     item_id INTEGER NOT NULL,
     quantity INTEGER NOT NULL,
     sale_date DATETIME NOT NULL,
-    cost DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (username) REFERENCES users(username),
     FOREIGN KEY (item_id) REFERENCES inventory(id)
 );
--- database/startingData.sql
+++ database/startingData.sql
@@ -10,14 +10,20 @@ VALUES ('llamport', '9171d14954eeda4e70777c23d98e349818125cdaeb884ff97ebf8cc0a9c
 INSERT into `users` (`username`, `password_hash`, `email`, `first_name`, `last_name`)
 VALUES ('bliskov', '1e4b9ae956cad1385cfa6fffd8323dd16c3fe18c54e6447e49bddef2138d042e84e1505a541c6ef19a5026e684b2559efd366145870a0a8d4d4173c0877f6cd2', 'barbara@thor.com', 'Barbara', 'Liskov');
 
-INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Apples', 'An edible cultivation of the Malus genus.', 2.00, 100, 'static/images/apple.jpeg', 'Fruit');
+INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`)
+VALUES ('lab 1', 'Lab 1 - Responce Reader.', 'static/images/lab1.jpeg', 'labs');
+
+INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`)
+VALUES ('lab 2', ' Lab 2 - Key Collection.', 'static/images/lab2.jpeg', 'labs');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Bananas', 'A long curved fruit which grows in clusters and has soft pulpy flesh and yellow skin when ripe.', 1.00, 100, 'static/images/banana.jpeg', 'Fruit');
+VALUES ('lab 3', 'Lab 3 - Py Game GUI.',  'static/images/lab3.jpeg', 'labs');
 
-INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Mangos', 'The best fruit on the planet.', 4.00, 100, 'static/images/mango.jpeg', 'Fruit');
+INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`)
+VALUES ('lab 4', 'Lab 4 - Git and Paired Programming.', 'static/images/lab4.jpeg', 'labs');
+
+INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`)
+VALUES ('lab 5', 'Lab 5 - Py Game GUI.', 'static/images/lab5.jpeg', 'labs');
 
 INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `sale_date`, `cost`)
 VALUES ('1', 'aturing', '1', 10, '2022-12-21 7:30:30', 5.50);
