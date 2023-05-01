b5733cd60491af5c10d35d7251bda75f363cb57d
Beren H <batterchox@gmail.com>
2023-05-01 18:00:30 -0400

    Adding Labs 6, 7, 8

--- database/startingData.sql
+++ database/startingData.sql
@@ -11,22 +11,31 @@ INSERT into `users` (`username`, `password_hash`, `email`, `first_name`, `last_n
 VALUES ('bliskov', '1e4b9ae956cad1385cfa6fffd8323dd16c3fe18c54e6447e49bddef2138d042e84e1505a541c6ef19a5026e684b2559efd366145870a0a8d4d4173c0877f6cd2', 'barbara@thor.com', 'Barbara', 'Liskov');
 
 INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`, 'item_file')
-VALUES ('lab 0', 'Lab 0 - Syntax Review: .', 'static/images/lab0.jpg', 'labs', 'static/labs/lab0.zip');
+VALUES ('Lab 0', 'Syntax Review', 'static/images/lab0.jpg', 'labs', 'static/labs/lab0.zip');
 
 INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`, 'item_file')
-VALUES ('lab 1', 'Lab 1 - Responce Reader.', 'static/images/lab1.jpg', 'labs', 'static/labs/lab1.zip');
+VALUES ('Lab 1', 'Basic Development Environment Setup', 'static/images/lab1.jpg', 'labs', 'static/labs/lab1.zip');
 
 INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`, 'item_file')
-VALUES ('lab 2', ' Lab 2 - Key Collection.', 'static/images/lab2.jpg', 'labs', 'static/labs/lab2.zip');
+VALUES ('Lab 2', 'Types and Built-in Functions in Python', 'static/images/lab2.jpg', 'labs', 'static/labs/lab2.zip');
 
 INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`, 'item_file')
-VALUES ('lab 3', 'Lab 3 - Py Game GUI.',  'static/images/lab3.jpg', 'labs', 'static/labs/lab3.zip');
+VALUES ('Lab 3', 'Polygon Visualizer',  'static/images/lab3.jpg', 'labs', 'static/labs/lab3.zip');
 
 INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`, 'item_file')
-VALUES ('lab 4', 'Lab 4 - Git and Paired Programming.', 'static/images/lab4.jpg', 'labs', 'static/labs/lab4.zip');
+VALUES ('Lab 4', 'Git and Paired Programming', 'static/images/lab4.jpg', 'labs', 'static/labs/lab4.zip');
 
 INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`, 'item_file')
-VALUES ('lab 5', 'Lab 5 - Invoice Tests.', 'static/images/lab5.jpg', 'labs', 'static/labs/lab5.zip');
+VALUES ('Lab 5', 'Test-Driven Development', 'static/images/lab5.jpg', 'labs', 'static/labs/lab5.zip');
+
+INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`, 'item_file')
+VALUES ('Lab 6', 'Building a Flask Web Server', 'static/images/lab6.jpg', 'labs', 'static/labs/lab6.zip');
+
+INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`, 'item_file')
+VALUES ('Lab 7', 'Automated Web Interactions with Selenium', 'static/images/lab7.jpg', 'labs', 'static/labs/lab7.zip');
+
+INSERT into `inventory` (`item_name`, `info`, `image_url`, `category`, 'item_file')
+VALUES ('Lab 8', 'Data Formats and Flow', 'static/images/lab8.jpg', 'labs', 'static/labs/lab8.zip');
 
 INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `sale_date`, `cost`)
 VALUES ('1', 'aturing', '1', 10, '2022-12-21 7:30:30', 5.50);
