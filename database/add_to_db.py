69af3244114f63e39552213ae6a61fa82203ff65
TylerDunphyCLT <tylerdunphy92@gmail.com>
2023-08-02 23:02:30 -0400

    testing addtodb

--- database/add_to_db.py
+++ database/add_to_db.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from database.db import Database
 
-foods = pd.read_csv('foods_table.csv')
+def csv_to_database():
+    """
+    A function that imports foods_table.csv into the store_records.db file via Database
 
-for index, row in foods.iterrows():
+    """
 
-    Database.insert_new_item(row['item_name'],row['info'],row['calorie'],row['protein'],row['carbs'],row['allergy'])
+    foods = pd.read_csv('foods_table.csv')
+    for index, row in foods.iterrows():
+        Database.insert_new_item(row['item_name'],row['info'],row['calorie'],row['protein'],row['carbs'],row['allergy'])
+
+
 
 
