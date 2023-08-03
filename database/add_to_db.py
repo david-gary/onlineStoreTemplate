efb737cb669497c3b4cf2f47ebc2693ceeb07f80
TylerDunphyCLT <tylerdunphy92@gmail.com>
2023-08-02 22:50:19 -0400

    add_to_db.py beginnings

--- database/add_to_db.py
+++ database/add_to_db.py
@@ -1,3 +1,10 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from database.db import Database
+
+foods = pd.read_csv('foods_table.csv')
+
+for index, row in foods.iterrows():
+
+    Database.insert_new_item(row['item_name'],row['info'],row['calorie'],row['protein'],row['carbs'],row['allergy'])
+
 
