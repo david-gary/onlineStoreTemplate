de9ae75ad17c240413a451155431ffa78731b8b8
Elijah Robinson <erob2018unc23@yahoo.com>
2023-08-07 22:09:44 -0400

    Inventory tests completed.

--- testing/inv_tests.py
+++ testing/inv_tests.py
@@ -10,6 +10,12 @@ def inv_empty():
     returns:
         - True if inventory is empty.
     """
+    db = Database("database/store_records.db")
+    if len(db.get_full_inventory()) == 0:
+        return True, "Inventory is empty"
+    else:
+        message = f"Inventory is not empty. Found {len(db.get_full_inventory())} items."
+        return False, message
 
 def inventory_contains_items():
     """
@@ -29,17 +35,40 @@ def inventory_contains_items():
         return False, message
     
 
-def inv_check():
+def inv_check_name():
     """
-    Tests that the inventory recognizes what is missing from inventory.
-    Checks what items are not fully stocked in inventory.
+    Tests that the inventory recognizes the name of an item from the inventory using its id.
 
     args:
         - None
 
     returns:
-        - True if inventory recognizes correct items that are not fully stocked.
+        - True if inventory recognizes item by its correct name.
     """
+    db = Database("database/store_records.db")
+    try:
+        db.get_item_name_by_id(10)
+        return True, "Item's name was found."
+    except:
+        return False, "Item's name was not found."
+    
+def inv_check_info():
+    """
+    Tests that the inventory recognizes the info of an item from the inventory using its id.
+
+    args:
+        - None
+
+    returns:
+        - True if inventory recognizes item by its correct info.
+    """
+    db = Database("database/store_records.db")
+    try:
+        db.get_item_info_by_id(10)
+        return True, "Item's info was found."
+    except:
+        return False, "Item's info was not found."
+    
 
 def inv_add():
     """
@@ -57,25 +86,3 @@ def inv_add():
         return True, "Item successfully added"
     except:
         return False, "Item failed to add"
-
-def inv_remove():
-    """
-    Tests that items can be removed from inventory.
-
-    args:
-        - None
-
-    returns:
-        - True if items can be removed from inventory.
-    """
-
-def inv_back():
-    """
-    Tests that user can click out of inventory check.
-
-    args:
-        - None
-
-    returns:
-        - True if user can exit successfully.
-    """
